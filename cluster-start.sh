e01ff84a75531bcd9a17291d3e17c63eec0470b1
mtweeman <mtweeman@gmail.com>
2022-03-23 21:32:07 +0100

    Changed scripts' names

--- cluster-start.sh
+++ cluster-start.sh
@@ -1,5 +1,5 @@
 PROFILE=hajle-silesia
-minikube start --driver=hyperv --memory 2000m --profile $PROFILE
+minikube start --driver=hyperv --memory 2.5g --profile $PROFILE
 minikube ssh -p $PROFILE -- '[ ! -d "/data/file_content_monitor" ] && sudo mkdir /data/file_content_monitor'
 
 kubectl apply -f file-content-monitor-pv.yml
