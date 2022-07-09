b929c392f9a6f3c094fe42d275c24af1972b3144
mtweeman <mtweeman@gmail.com>
2022-07-09 15:00:44 +0200

    Added storage for file-content-processor repo

--- cluster-start.sh
+++ cluster-start.sh
@@ -2,6 +2,7 @@ PROFILE=hajle-silesia
 minikube start --driver=hyperv --memory 2.5g --profile $PROFILE
 minikube ssh -p $PROFILE -- \
 '[ ! -d "/data/file_content_monitor" ] && sudo mkdir /data/file_content_monitor;
-[ ! -d "/data/file_content_converter" ] && sudo mkdir /data/file_content_converter;'
+[ ! -d "/data/file_content_converter" ] && sudo mkdir /data/file_content_converter;
+[ ! -d "/data/file_content_processor" ] && sudo mkdir /data/file_content_processor;'
 
 minikube -p $PROFILE mount "C:/recipe:/data/recipe" &
