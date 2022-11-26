45a21d4763ca76ee896d5a790ba8f33248748401
mtweeman <mtweeman@gmail.com>
2022-11-26 13:09:42 +0100

    Increased memory for Strimzi min. requirements

--- cluster-start.sh
+++ cluster-start.sh
@@ -1,5 +1,5 @@
 PROFILE=hajle-silesia
-minikube start --driver=hyperv --memory 2.5g --profile $PROFILE
+minikube start --driver=hyperv --memory 4g --profile $PROFILE
 minikube ssh -p $PROFILE -- \
 '[ ! -d "/data/file_content_monitor" ] && sudo mkdir /data/file_content_monitor;
 [ ! -d "/data/file_content_converter" ] && sudo mkdir /data/file_content_converter;
