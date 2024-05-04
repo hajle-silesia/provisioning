5a108cc73bca1163d345579c354f6c17368aceb5
mtweeman <mtweeman@gmail.com>
2024-05-04 21:14:23 +0200

    Migration to OCI: updated outputs, workflows and backend (#31)

--- nodes/servers/outputs.tf
+++ nodes/servers/outputs.tf
@@ -1,3 +1,3 @@
-output "servers_public_ips" {
+output "data" {
   value = {for instance_data in data.oci_core_instance.data : instance_data.display_name => instance_data.public_ip}
 }
--- outputs.tf
+++
@@ -1,3 +0,0 @@
-output "servers_public_ips" {
-  value = module.servers["eu-frankfurt-1"].servers_public_ips
-}
