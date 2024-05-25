4232da0f74d91809d09d277de453b004d1c18ab7
mtweeman <mtweeman@gmail.com>
2024-05-25 18:06:00 +0200

    Updates for Packer (#34)

--- outputs-ansible-hosts.tf
+++ outputs-ansible-hosts.tf
@@ -1,4 +1,4 @@
-output "servers" {
+output "server" {
   value = {
     "hosts" : {
       for server_name, public_ip in module.servers["eu-frankfurt-1"].data : server_name => { ansible_host = public_ip }
