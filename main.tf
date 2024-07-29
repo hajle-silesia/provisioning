0ac6936e9d1319bfba4aeb76d50062b196d768d5
mtweeman <mtweeman@gmail.com>
2024-07-29 15:40:29 +0200

    Updated according to Terraform style guide (#65)

--- main.tf
+++ main.tf
@@ -1,35 +1,3 @@
-terraform {
-  required_providers {
-    oci = {
-      source  = "hashicorp/oci"
-      version = "~> 5.0"
-    }
-  }
-
-  backend "s3" {
-    bucket                      = "backend"
-    region                      = "eu-frankfurt-1"
-    key                         = "state/terraform.tfstate"
-    skip_region_validation      = true
-    skip_credentials_validation = true
-    skip_requesting_account_id  = true
-    use_path_style              = true
-    skip_metadata_api_check     = true
-    skip_s3_checksum            = true
-    endpoints = {
-      s3 = "https://fru7nifnglug.compat.objectstorage.eu-frankfurt-1.oraclecloud.com"
-    }
-  }
-}
-
-provider "oci" {
-  tenancy_ocid     = var.tenancy_ocid
-  user_ocid        = var.user_ocid
-  private_key_path = var.private_key_path
-  fingerprint      = var.fingerprint
-  region           = var.region
-}
-
 module "network" {
   source             = "./network"
   compartment_ocid   = var.compartment_ocid
