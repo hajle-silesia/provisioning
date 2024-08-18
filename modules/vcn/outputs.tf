2a9203bbc9ee5c7366e887b0f4cb9cc3d93f9c9b
mtweeman <mtweeman@gmail.com>
2024-08-18 20:06:54 +0200

    Create security lists to handle SSH

--- modules/vcn/outputs.tf
+++ modules/vcn/outputs.tf
@@ -7,3 +7,8 @@ output "igw_id" {
   value       = try(oci_core_internet_gateway.default[0].id, "")
   description = "The ID of the internet gateway"
 }
+
+output "default_security_list_id" {
+  value       = oci_core_vcn.default[0].default_security_list_id
+  description = "The ID of the default security list"
+}
