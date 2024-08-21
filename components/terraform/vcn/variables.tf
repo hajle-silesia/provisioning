2eef50c679eefe073c95dbf1f04b56b2cd493212
mtweeman <mtweeman@gmail.com>
2024-08-21 10:00:34 +0200

    Group context vars + improve VCN module outputs (#81)

--- components/terraform/vcn/variables.tf
+++ components/terraform/vcn/variables.tf
@@ -18,30 +18,19 @@ variable "region" {
   type = string
 }
 
-variable "enabled" {
-  type = bool
-}
-
-variable "namespace" {
-  type = string
-}
-
-variable "stage" {
-  type = string
-}
-
-variable "environment" {
-  type = string
-}
-
-variable "name" {
-  type = string
+variable "context" {
+  type = object({
+    enabled     = bool
+    namespace   = string
+    stage       = string
+    environment = string
+    name        = string
+  })
 }
 
 variable "compartment_ocid" {
   type        = string
   description = "Compartment OCID"
-  default     = null
 }
 
 variable "vcn" {
--- machine-images/imported-vars.pkr.hcl
+++
@@ -1,6 +0,0 @@
-# This file is generated automatically
-
-variable "internal_lb" {
-  type    = string
-  default = "10.20.41.254"
-}
