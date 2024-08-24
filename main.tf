98458b88ab3f96076360117eedb509c21106d61a
mtweeman <mtweeman@gmail.com>
2024-08-24 13:49:11 +0200

    Fix Terraform validate issues (#86)

--- main.tf
+++ main.tf
@@ -7,4 +7,7 @@ module "servers" {
   shape                  = var.shape
   vault_cert_private_key = var.vault_cert_private_key
   subnet_id              = module.vcn_reference.outputs.subnet_id
+  instance_public_key    = var.instance_public_key
+  vault_ca_cert          = var.vault_ca_cert
+  vault_cert             = var.vault_cert
 }
--- variables.tf
+++ variables.tf
@@ -45,6 +45,18 @@ variable "shape" {
   type = string
 }
 
+variable "instance_public_key" {
+  type = string
+}
+
 variable "vault_cert_private_key" {
+  type = string
+}
+
+variable "vault_ca_cert" {
+  type = string
+}
+
+variable "vault_cert" {
   type = string
 }
