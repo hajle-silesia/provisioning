702961790be6d39f00473ce6cfda19373a4c1ef8
mtweeman <mtweeman@gmail.com>
2024-10-28 20:32:30 +0100

    fix: add internal LB IP address output

--- components/terraform/alb/outputs.tf
+++ components/terraform/alb/outputs.tf
@@ -12,3 +12,8 @@ output "internal_lb_id" {
   value       = oci_load_balancer_load_balancer.internal.id
   description = "The ID of the internal LB"
 }
+
+output "internal_lb_ip_address" {
+  value       = oci_load_balancer_load_balancer.internal.ip_address_details[0].ip_address
+  description = "The IP address of the internal LB"
+}
