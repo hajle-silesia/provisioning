3662783167dd04aaa163c47e6f490ae627edc5d1
mtweeman <mtweeman@gmail.com>
2024-10-29 11:20:35 +0100

    feat: manage instances via Atmos

---
+++ components/terraform/instance/machine-images/imported-vars.pkr.hcl
@@ -0,0 +1,7 @@
+# This file is generated automatically
+
+variable "internal_lb" {
+  type    = string
+  default = "10.20.82.196"
+}
+
---
+++ components/terraform/instance/variables.tf
@@ -0,0 +1,51 @@
+variable "tenancy_ocid" {
+  type = string
+}
+
+variable "user_ocid" {
+  type = string
+}
+
+variable "private_key_path" {
+  type = string
+}
+
+variable "fingerprint" {
+  type = string
+}
+
+variable "region" {
+  type = string
+}
+
+variable "compartment_ocid" {
+  type = string
+}
+
+variable "enabled" {
+  type = bool
+}
+
+variable "namespace" {
+  type = string
+}
+
+variable "stage" {
+  type = string
+}
+
+variable "environment" {
+  type = string
+}
+
+variable "name" {
+  type = string
+}
+
+variable "shape" {
+  type = string
+}
+
+variable "instance_public_key" {
+  type = string
+}
