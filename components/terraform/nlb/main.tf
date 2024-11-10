2f65f1b436803b75489626e40737241d1e72d6f6
mtweeman <mtweeman@gmail.com>
2024-11-10 18:20:10 +0000

    fix: remove redundant IP address from nlb

--- components/terraform/nlb/main.tf
+++ components/terraform/nlb/main.tf
@@ -1,10 +1,9 @@
 module "nlb" {
   source = "../../../modules/nlb"
 
-  compartment_ocid      = var.compartment_ocid
-  name                  = var.nlb.name
-  subnet_id             = module.vcn_reference.outputs.subnet_id
-  assigned_private_ipv4 = var.nlb.assigned_private_ipv4
-  listener_port         = var.nlb.listener_port
-  health_check_port     = var.nlb.health_check_port
+  compartment_ocid  = var.compartment_ocid
+  name              = var.nlb.name
+  subnet_id         = module.vcn_reference.outputs.subnet_id
+  listener_port     = var.nlb.listener_port
+  health_check_port = var.nlb.health_check_port
 }
