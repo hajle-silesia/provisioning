e90638280d34d84290d7b0fb0d48cfb320593512
mtweeman <mtweeman@gmail.com>
2024-12-04 09:35:50 +0100

    feat: add port output to listener component and module (#352)

--- components/terraform/alb-listener/outputs.tf
+++ components/terraform/alb-listener/outputs.tf
@@ -2,3 +2,8 @@ output "backend_set_name" {
   value       = module.alb_listener.backend_set_name
   description = "The name of the backend set"
 }
+
+output "port" {
+  value       = module.alb_listener.port
+  description = "The port of the listener"
+}
--- modules/alb-listener/outputs.tf
+++ modules/alb-listener/outputs.tf
@@ -2,3 +2,8 @@ output "backend_set_name" {
   value       = join("", oci_load_balancer_backend_set.default[*].name)
   description = "The name of the backend set"
 }
+
+output "port" {
+  value       = join("", oci_load_balancer_listener.default[*].port)
+  description = "The port of the listener"
+}
